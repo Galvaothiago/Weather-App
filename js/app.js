70879b3388426295bbc8e0ca71cbe96523c7f171
thiago <thiagosilva128@gmail.com>
2021-01-23 20:28:24 -0300

    init commit

---
+++ js/app.js
@@ -0,0 +1,69 @@
+const input = document.querySelector('.input-custom')
+const showInput = document.querySelector('.add-input')
+const form = document.querySelector('[data-js="change-location"]')
+const backgroudIsDay = document.querySelector('[data-js="background"]')
+const weatherIconContainer = document.querySelector('[data-js="weather-icon"]')
+
+const cityNameContainer = document.querySelector('[data-js="city-name"]')
+const weatherTextContainer = document.querySelector('[data-js="weather-text"]')
+const temperatureContainer = document.querySelector('[data-js="temperature"]')
+const lastUpdate = document.querySelector('[data-js="last-update"]')
+
+const city = localStorage.getItem('city')
+
+const changeBackgroundWeather = isDay => {
+    if (isDay) {
+        return backgroudIsDay.style.backgroundImage = "url(./src/day.png)" 
+    }
+    backgroudIsDay.style.backgroundImage = "url(./src/night3.jpg)"
+}
+
+const getWeatherInfo = async inputValue => {
+    const [{ WeatherText, Temperature, WeatherIcon, IsDayTime, LocalObservationDateTime}] = await getWeatherData(inputValue)
+    const { LocalizedName } = await getCityData(inputValue)
+    const { AdministrativeArea } = await getCityData(inputValue)
+
+    const lastUpdateGetWeatherHours = LocalObservationDateTime.split('T')[1].split('-')[0]
+
+    cityNameContainer.textContent = `${ LocalizedName.toUpperCase() }, ${ AdministrativeArea.LocalizedName.toUpperCase() }`
+    weatherTextContainer.textContent = WeatherText
+    temperatureContainer.textContent = Temperature.Metric.Value
+    lastUpdate.innerHTML = `Ultima coleta: ${lastUpdateGetWeatherHours}`
+
+    changeBackgroundWeather(IsDayTime)
+
+    weatherIconContainer.src = `./src/icon/${WeatherIcon}.svg`
+}
+
+
+window.addEventListener('load', () => {
+
+    if (!city) {
+        getWeatherInfo('Vinhedo')
+    }
+})
+
+showInput.addEventListener('click', () => {
+    input.classList.remove('d-none')
+    input.focus()
+})
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const inputValue = e.target.city.value.trim()
+
+    getWeatherInfo(inputValue)
+    form.reset()
+    input.classList.add('class', 'd-none')
+
+    localStorage.setItem('city', inputValue)   
+})
+
+
+
+if (city) {
+    getWeatherInfo(city)
+}
+
+// console.log(getWeatherInfo)
---
+++ js/wheather.js
@@ -0,0 +1,35 @@
+
+const APIKey = 'xx4PxfnVXfmiAVxoAILd7zAHHKGn4zQw'
+const cityUrl = cityName => 
+    `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
+
+const getCityData = async cityName => {
+    try{
+        const response = await fetch(cityUrl(cityName))
+
+        if (!response.ok) {
+            throw new Error('Não foi possivel obter as informações solicitadas')
+        }
+        const [cityData] = await response.json()
+        return cityData
+    } catch ({ name, message }) {
+        alert(`${name}: ${message}`)
+        
+    }
+}
+
+const getWeatherData = async cityName => {
+    try {
+        const cityKey = await getCityData(cityName)
+        const result = await fetch(`http://dataservice.accuweather.com/currentconditions/v1/${cityKey.Key}?apikey=${APIKey}&language=pt-br`)
+
+        console.log(cityKey)
+        if (!result.ok) {
+            throw new Error('Não foi possivel obter os dados')
+        }
+
+        return result.json()
+    } catch ({ name, message }) {
+        alert(`nao foi possivel obter os dados`)
+}
+}
